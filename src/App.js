2275b953cf8e596cdcb09424b8a01adc3f9f7feb
Pob <590650@gmail.com>
2020-09-12 19:57:52 +0700

    changed Text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World</h1>
+        <h1>Howdy, You</h1>
       </header>
       <AmplifySignOut />
     </div>
